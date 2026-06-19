9b39dc548eba9f8f7fd655d984e4161069d57c56
JeS24 <jyotirmaya.shivottam@niser.ac.in>
2026-06-19 17:22:20 +0530

    cleaning up files that should never be added to git

--- run-backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0backend"
-"%~dp0backend\venv\Scripts\python.exe" manage.py runserver 127.0.0.1:8000
-pause
--- run-frontend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0frontend"
-npm.cmd run dev -- --configLoader native --host 127.0.0.1 --port 5173
-pause
